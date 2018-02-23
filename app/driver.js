2c309efe34b05fd8c58953c731a431a2e7ad34d6
Guy Wassather <ggwass@gmail.com>
2018-02-23 12:11:31 -0800

    Add composite view for form input

--- app/driver.js
+++ app/driver.js
@@ -15,11 +15,53 @@ const ToDo = Marionette.LayoutView.extend({
   template: require('./templates/todoitem.html')
 });
 
-const TodoList =  Marionette.CollectionView.extend({
-  el: '#app-hook',
-  tagName: 'ul',
+// CollectionView loops through all models in specified collection, rendering
+// each ofthem using a specific itemView (childView?--this must be specified
+// for CollectionView), then appends the resuls of the item view's `el` to the
+// collection view's `el`
+// const TodoList =  Marionette.CollectionView.extend({
+//   el: '#app-hook',
+//   tagName: 'ul',
+//
+//   childView: ToDo
+// });
 
-  childView: ToDo
+// CompositeView extends from CollectionView to be used as a composite view
+// for scenarios where it should represent both a branch and a leaf in a tree
+// structure, or for scenarios where a collection needs to be rendered within
+// a wrapper template
+const TodoList =  Marionette.CompositeView.extend({
+  el: '#app-hook',
+  template: require('./templates/todoitem.html'),
+
+  childView: ToDo,
+  childViewContainer: 'ul',
+
+  ui: {
+    assignee: '#id_assignee',
+    form: 'form',
+    text: '#id_text'
+  },
+
+  triggers: {
+    'submit @ui.form': 'add:todo:item'
+  },
+
+  collectionEvents: {
+    add: 'itemAdded'
+  },
+
+  onAddTodoItem: function() {
+    this.collection.add({
+      asignee: this.ui.assignee.val(),
+      text: this.ui.text.val(),
+    });
+  },
+
+  itemAdded: function() {
+    this.ui.assignee.val('');
+    this.ui.text.val('');
+  }
 });
 
 const todo = new TodoList({
