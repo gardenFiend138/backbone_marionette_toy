2399145af47080b8da03a12855474a9da8c083db
Guy Wassather <ggwass@gmail.com>
2018-02-23 10:55:33 -0800

    Start todo application

--- app/driver.js
+++ app/driver.js
@@ -1,5 +1,5 @@
-var Backbone = require('backbone');
-var Marionette = require('backbone.marionette'); // import Marionette
+const Backbone = require('backbone');
+const Marionette = require('backbone.marionette'); // import Marionette
 
 
 // var HelloWorld = Marionette.LayoutView.extend({ // create new view
@@ -10,3 +10,20 @@ var Marionette = require('backbone.marionette'); // import Marionette
 // var hello = new HelloWorld();                   // instantiate new instance of view
 //
 // hello.render();                                 // render the new instance of view
+
+const TodoList =  Marionette.LayoutView.extend({
+  el: '#app-hook',
+  template: require('./templates/layout.html')
+});
+
+const todo = new TodoList({
+  model: new Backbone.Model({ // wrapping todo object in backbone model 
+    items: [
+      {assignee: 'Guy', text: 'Learn backbone'},
+      {assignee: 'Guy', text: 'Learn marionette'},
+      {assignee: 'Me', text: 'Have fun!'},
+    ]
+  })
+});
+
+todo.render();
